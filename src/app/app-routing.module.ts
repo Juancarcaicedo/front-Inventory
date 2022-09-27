708ab860d2d2bb8655f97c9eda8e96d9daf39eee
juancarcaicedo <juancarcaicedo@unicauca.edu.co>
2022-09-26 21:04:11 -0500

    Se Agrega el proyecto Front primera version

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardRoutingModule } from './modules/dashboard/dashboard-routing.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,
+    {enableTracing: false, useHash: true}
+    ),
+    DashboardRoutingModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+ }
+// Archivo de ruta principales 
---
+++ src/app/modules/dashboard/router-child.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+
+
+const childroutes: Routes=[
+   { path: '', component: HomeComponent },
+   { path: 'home', component: HomeComponent }
+]
+
+
+@NgModule({
+    imports: [RouterModule.forChild(childroutes)],
+    exports: [RouterModule]
+   
+})
+export class RouterChildModule { }
